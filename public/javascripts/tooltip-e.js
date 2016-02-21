3f4c739f561adee8f35cc043ae96b2cb5a19fedf
Damian <d.ezequiel.palacios@gmail.com>
2016-02-21 20:05:32 -0300

    datepicker

--- public/javascripts/tooltip-e.js
+++ public/javascripts/tooltip-e.js
@@ -2,3 +2,27 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip();
   console.log("tooltip enabled");
 });
+
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+var checkin = $('#date_in').datepicker({
+  onRender: function(date) {
+    return date.valueOf() < now.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  if (ev.date.valueOf() > checkout.date.valueOf()) {
+    var newDate = new Date(ev.date)
+    newDate.setDate(newDate.getDate() + 1);
+    checkout.setValue(newDate);
+  }
+  checkin.hide();
+  $('#date_out')[0].focus();
+}).data('datepicker');
+var checkout = $('#date_out').datepicker({
+  onRender: function(date) {
+    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  checkout.hide();
+}).data('datepicker');
