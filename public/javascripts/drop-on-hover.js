3337d26c910a26ba25abdb7d1738a5abfe10236a
Damian Palacios <Damian Palacios>
2016-03-15 18:20:38 -0300

    responsive del navbar, unificacion de js

--- public/javascripts/drop-on-hover.js
+++ public/javascripts/drop-on-hover.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
+
+	//Dropdownb on hover event
 	$(".dropdown, .btn-group").hover(function(){
 		var dropdownMenu = $(this).children(".dropdown-menu");
 		if(dropdownMenu.is(":visible")){
 			dropdownMenu.parent().toggleClass("open");
 		}
 	});
-  $(".drop-fix").click(function() {
-    var item = $(this);
-    var url = item.data('url')
-    window.location.href='/'+ url;
-  });
+
+	//Disable Dropdown on hover in XS Resolution}
+	if($(window).width()>768){
+		$(".drop-fix").click(function() {
+			var item = $(this);
+			var url = item.data('url');
+			window.location.href='/'+ url;
+		});
+	}
+
+	//Show promocodes input
+	$( '.promocode-t' ).click(function() {
+		$( '.promocode-i' ).toggle(400, 'swing');
+	});
+
+	// Fix tooltip issues
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip({container:'body'});
+	});
 });
- 
--- public/javascripts/tooltip-e.js
+++ public/javascripts/tooltip-e.js
@@ -1,11 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip({container:'body'});
-  console.log("tooltip enabled");
 });
-//blueimp.Gallery(
-//    document.getElementById('links').getElementsByTagName('a'),
-//    {
-//        container: '#blueimp-gallery-carousel',
-//        carousel: true
-//    }
-//);
