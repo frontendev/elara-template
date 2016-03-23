128a37c9d2c704b102bdbe2748c99344c0c839f8
Damian Palacios <Damian Palacios>
2016-03-23 17:02:23 -0300

    hola lucas :D

--- routes/general.js
+++ routes/general.js
@@ -8,28 +8,31 @@ var siteService = require('../services/sites');
 
 var indexAction = function (structure,res){
   var nodoRaiz  = ss.getRootNode();
-
     res.render('index', { title: 'HTL Suica Hotel & Resort - Home', url : "/", menu_items: structure[nodoRaiz].hijos });
 };
 
-var roomAction = function (structure,res){
+var roomsAction = function (structure,res){
   //solicitar habitaciones
   var rooms = null;
-    res.render('rooms', { title: 'Habitaciones - HTL Suica Hotel & Resort', url : "/habitaciones"});
+  res.render('rooms', { title: 'Habitaciones - HTL Suica Hotel & Resort', url : "/habitaciones"});
 };
 
 var hotelAction = function (structure,res){
-    res.render('index', { title: 'Habitaciones - HTL Suica Hotel & Resort', url : "/habitaciones"});
+  res.render('index', { title: 'Habitaciones - HTL Suica Hotel & Resort', url : "/habitaciones"});
 };
 
 var roomAction = function (structure,res){
-    res.render('room', { title: 'Habitacion - HTL Suica Hotel & Resort', url : "/habitacion"});
+  res.render('room', { title: 'Habitacion - HTL Suica Hotel & Resort', url : "/habitacion"});
 };
 
 var contactAction = function (structure,res){
-    res.render('contact', { title: 'Contacto - HTL Suica Hotel & Resort', url : "/contacto"});
+  res.render('contact', { title: 'Contacto - HTL Suica Hotel & Resort', url : "/contacto"});
 };
-
+/*
+var contactAction = function (structure,res){
+  res.render('contact', { title: 'Contacto - HTL Suica Hotel & Resort', url : "/contacto"});
+};
+*/
 
 router.get(/^.*$/, function(req,res,next) {
   //- Static URL
@@ -40,7 +43,7 @@ router.get(/^.*$/, function(req,res,next) {
   if( sr.isASitePath()){
     var path =  sr.getClearUrl();
     console.log('path: '+path);
-    ss = new siteService('https://ra.mbooking.com.ar/','HTL0000001',3);
+    ss = new siteService('https://ra.mbooking.com.ar/','ALP0000001',1);
     ss.getPathRequest(path,function(structure) {
         var action = ss.getAction()+'Action';
         try{
--- services/sites.js
+++ services/sites.js
@@ -10,7 +10,7 @@ var $parIdDistribucion =[{ 1: "index"}, { 2: "hotel"}, { 3: "room"}, {4: "servic
    */
 
   var setDistribucion = function (distribucionId ){
-    var $distribuciones =[{"distribucion_id": 1,"nombre": "Inicio","vista": "index"}, {"distribucion_id": 2,"nombre": "Hotel","vista": "hotel"}, {"distribucion_id": 3,"nombre": "Habitacion","vista": "room"}, {"distribucion_id": 4,"nombre": "Servicios","vista": "services"}, {"distribucion_id": 5,"nombre": "Contacto","vista": "contact"}, {"distribucion_id": 6,"nombre": "Ubicacion","vista": "location"}, {"distribucion_id": 7,"nombre": "Promocion","vista": "promotion"}, {"distribucion_id": 8,"nombre": "Encuesta","vista": "encuesta"}, {"distribucion_id": 9,"nombre": "Fotos","vista": "gallery"}, {"distribucion_id": 10,"nombre": "Legales","vista": "legal"}, {"distribucion_id": 11,"nombre": "Llamenos","vista": "callus"}, {"distribucion_id": 12,"nombre": "360","vista": "gallery360"}, {"distribucion_id": 13,"nombre": "HomeCadena","vista": "homechain"}, {"distribucion_id": 14,"nombre": "Recreacion","vista": "recreation"}, {"distribucion_id": 15,"nombre": "Contacto2","vista": "contact2"}, {"distribucion_id": 16,"nombre": "Registro","vista": "register"}];
+    var $distribuciones =[{"distribucion_id": 1,"nombre": "Inicio","vista": "index"}, {"distribucion_id": 2,"nombre": "Hotel","vista": "hotel"}, {"distribucion_id": 3,"nombre": "Habitacion","vista": "rooms"}, {"distribucion_id": 4,"nombre": "Servicios","vista": "services"}, {"distribucion_id": 5,"nombre": "Contacto","vista": "contact"}, {"distribucion_id": 6,"nombre": "Ubicacion","vista": "location"}, {"distribucion_id": 7,"nombre": "Promocion","vista": "promotion"}, {"distribucion_id": 8,"nombre": "Encuesta","vista": "encuesta"}, {"distribucion_id": 9,"nombre": "Fotos","vista": "gallery"}, {"distribucion_id": 10,"nombre": "Legales","vista": "legal"}, {"distribucion_id": 11,"nombre": "Llamenos","vista": "callus"}, {"distribucion_id": 12,"nombre": "360","vista": "gallery360"}, {"distribucion_id": 13,"nombre": "HomeCadena","vista": "homechain"}, {"distribucion_id": 14,"nombre": "Recreacion","vista": "recreation"}, {"distribucion_id": 15,"nombre": "Contacto2","vista": "contact2"}, {"distribucion_id": 16,"nombre": "Registro","vista": "register"}];
     for( var i in $distribuciones){
       if($distribuciones[i].distribucion_id == distribucionId){
           $action = $distribuciones[i].vista;
@@ -32,14 +32,16 @@ var $parIdDistribucion =[{ 1: "index"}, { 2: "hotel"}, { 3: "room"}, {4: "servic
   };
 
     var responseHandler = function(error,result){
-        if(result.error != undefined){
+        if(!result || result.error != undefined){
             console.log('Code:'+result.error.code);
             console.log('Message:'+result.error.exception[0].message);
             for(line in result.error.exception[0].trace){
                 var errorLine = result.error.exception[0].trace[line];
                 console.log('   ->'+errorLine.file +' ('+errorLine.line+')');
             }
-            //throw new Exception('Error en la respuesta del servidor ');
+            return false;
+        }else{
+            return true;
         }
     };
 
@@ -48,10 +50,14 @@ var $parIdDistribucion =[{ 1: "index"}, { 2: "hotel"}, { 3: "room"}, {4: "servic
         var url = $serviceUrl+'api/sites/'+$siteId+'?bookId='+$bookId+'&path='+path;
         rest.get(url)
         .on('complete', function(result) {
-          var keys = Object.keys( result.structure);
-          setRootNode(keys[0]);
-          setDistribucion(result.structure[keys[keys.length-1]].distribucion_id);
-          callback(result.structure);
+            if(responseHandler(null,result)){
+                if(result != 'undefined'){
+                  var keys = Object.keys( result.structure);
+                  setRootNode(keys[0]);
+                  setDistribucion(result.structure[keys[keys.length-1]].distribucion_id);
+                  callback(result.structure);
+                }
+            }
         })
         .on('error',responseHandler);
 
